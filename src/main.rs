dfe00d0388a86d3618928d189d8a91e78d436939
Dmitri Melnikov <github@dmit.me>
2018-12-26 23:07:10 +0200

    [Ch 6] Implement anti-aliasing

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 use image;
+use rand::prelude::*;
 use std::env;
 
 use tachibana::color::Color;
-use tachibana::ray::Ray;
+use tachibana::ray::{Camera, Ray};
 use tachibana::shape::{Shape, Shapes, Sphere};
 use tachibana::vec::Vec3;
 
-fn color(r: &Ray, world: &Shapes) -> Color {
+fn color_vec(r: &Ray, world: &Shapes) -> Vec3 {
     if let Some(rec) = world.hit(r, 0., std::f64::MAX) {
-        let c = Vec3 {
+        Vec3 {
             x: rec.normal.x + 1.,
             y: rec.normal.y + 1.,
             z: rec.normal.z + 1.,
-        } * 0.5;
-        c.into()
+        } * 0.5
     } else {
         let unit_direction = r.direction.unit();
         let t = (unit_direction.y + 1.) * 0.5;
@@ -22,13 +22,12 @@ fn color(r: &Ray, world: &Shapes) -> Color {
             y: 0.7,
             z: 1.,
         };
-        let c = Vec3::ONE * (1. - t) + color * t;
-        c.into()
+        Vec3::ONE * (1. - t) + color * t
     }
 }
 
 fn main() {
-    let mut args = env::args().skip(1).take(2).map(|x| x.parse::<u32>());
+    let mut args = env::args().skip(1).take(3).map(|x| x.parse::<u32>());
     let width: u32 = match args.next() {
         Some(Ok(w)) => w,
         Some(Err(e)) => panic!("Invalid format for image width: {}", e),
@@ -39,21 +38,10 @@ fn main() {
         Some(Err(e)) => panic!("Invalid format for image height: {}", e),
         _ => width / 2,
     };
-
-    let lower_left_corner = Vec3 {
-        x: -2.,
-        y: -1.,
-        z: -1.,
-    };
-    let horizontal = Vec3 {
-        x: 4.,
-        y: 0.,
-        z: 0.,
-    };
-    let vertical = Vec3 {
-        x: 0.,
-        y: 2.,
-        z: 0.,
+    let num_rays: u32 = match args.next() {
+        Some(Ok(n)) => n,
+        Some(Err(e)) => panic!("Invalid format for number of rays: {}", e),
+        _ => 100,
     };
 
     #[rustfmt::skip]
@@ -65,16 +53,21 @@ fn main() {
     };
 
     let mut buf = image::ImageBuffer::new(width, height);
+    let camera = Camera::default();
+    let mut rng = rand::thread_rng();
 
     for y in 0..height {
         for x in 0..width {
-            let u = f64::from(x) / f64::from(width);
-            let v = f64::from(y) / f64::from(height);
-            let ray = Ray {
-                origin: Vec3::ZERO,
-                direction: lower_left_corner + horizontal * u + vertical * v,
+            let color: Color = {
+                let c = (0..num_rays).fold(Vec3::ZERO, |acc, _| {
+                    let u = (f64::from(x) + rng.gen::<f64>()) / f64::from(width);
+                    let v = (f64::from(y) + rng.gen::<f64>()) / f64::from(height);
+                    let ray = camera.ray(u, v);
+                    let c = color_vec(&ray, &shapes);
+                    acc + c
+                }) / f64::from(num_rays);
+                c.into()
             };
-            let color = color(&ray, &shapes);
 
             let p = buf.get_pixel_mut(x, height - y - 1); // write image starting from the bottom row
             *p = image::Rgb(color.into());
--- src/ray.rs
+++ src/ray.rs
@@ -11,3 +11,33 @@ impl Ray {
         self.origin + self.direction * t
     }
 }
+
+#[derive(Clone, Copy, Debug)]
+pub struct Camera {
+    pub origin: Vec3,
+    pub lower_left_corner: Vec3,
+    pub horizontal: Vec3,
+    pub vertical: Vec3,
+}
+
+impl Camera {
+    pub fn ray(&self, u: f64, v: f64) -> Ray {
+        Ray {
+            origin: self.origin,
+            direction: self.lower_left_corner + self.horizontal * u + self.vertical * v
+                - self.origin,
+        }
+    }
+}
+
+impl Default for Camera {
+    #[rustfmt::skip]
+    fn default() -> Self {
+        Camera {
+            origin           : Vec3 { x:  0., y:  0., z:  0. },
+            lower_left_corner: Vec3 { x: -2., y: -1., z: -1. },
+            horizontal       : Vec3 { x:  4., y:  0., z:  0. },
+            vertical         : Vec3 { x:  0., y:  2., z:  0. },
+        }
+    }
+}
